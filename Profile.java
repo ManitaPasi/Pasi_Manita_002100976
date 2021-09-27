9f961d652c0827285d11ed217cb53ae16df545b0
ManitaPasi <91227128+ManitaPasi@users.noreply.github.com>
2021-09-26 22:54:16 -0400

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,178 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author dell
+ */
+public class Profile {
+    
+    private String name;
+    private String address;
+    private String dob;
+    private String tel;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String medicalrecord;
+    private String bankacc;
+    private String licence;
+    private String licenseplateno;
+    private String linkedin;
+    private String ipaddress;
+    private String uniqueidentifier;
+    
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTel() {
+        return this.tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getFax() {
+        return this.fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return this.ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalrecord() {
+        return this.medicalrecord;
+    }
+
+    public void setMedicalrecord(String medicalrecord) {
+        this.medicalrecord = medicalrecord;
+    }
+
+    public String getBankacc() {
+        return this.bankacc;
+    }
+
+    public void setBankacc(String bankacc) {
+        this.bankacc = bankacc;
+    }
+
+    public String getLicence() {
+        return this.licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence;
+    }
+
+    public String getLicenseplateno() {
+        return this.licenseplateno;
+    }
+
+    public void setLicenseplateno(String licenseplateno) {
+        this.licenseplateno = licenseplateno;
+    }
+
+    public String getLinkedin() {
+        return this.linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpaddress() {
+        return this.ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getUniqueidentifier() {
+        return this.uniqueidentifier;
+    }
+
+    public void setUniqueidentifier(String uniqueidentifier) {
+        this.uniqueidentifier = uniqueidentifier;
+    }
+
+    public String getSSN() {
+        return this.ssn;
+    }
+
+    public String getMedicalRecord() {
+        
+          return this.medicalrecord;
+//        throw new UnsupportedOperationException("Not supported yet.");  //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getBankAcc() {
+        
+        return this.bankacc;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getLicense() {
+        return this.licence;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getLicensePlateNo() {
+        return this.licenseplateno;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getIpAddress() {
+        return this.ipaddress;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getUniqueIdentifier() {
+        return this.uniqueidentifier;
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
